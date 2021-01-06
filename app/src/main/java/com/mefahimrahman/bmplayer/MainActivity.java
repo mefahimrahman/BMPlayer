193e18ddd73ff97a4c195216f58b1a61debb5353
mefahimrahman <mefahimrahman@gmail.com>
2021-01-06 22:52:55 +0600

    BMPlayer v1 completed
    1. Added ActionListener on 3 Button for Play, Pause and Stop Music
    2. Test Music Hardcoded in raw folder

--- app/src/main/java/com/mefahimrahman/bmplayer/MainActivity.java
+++ app/src/main/java/com/mefahimrahman/bmplayer/MainActivity.java
@@ -2,13 +2,58 @@ package com.mefahimrahman.bmplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton play, pause, stop;
+    MediaPlayer mediaPlayer;
+    int CurrentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = (ImageButton) findViewById(R.id.playButton);
+        pause = (ImageButton) findViewById(R.id.pauseButton);
+        stop = (ImageButton) findViewById(R.id.stopButton);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.playButton:
+                if(mediaPlayer == null) {
+                    mediaPlayer  = MediaPlayer.create(getApplicationContext(), R.raw.got);
+                    mediaPlayer.start();
+                }
+                else if(!mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(CurrentPosition);
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.pauseButton:
+                if(mediaPlayer != null) {
+                    mediaPlayer.pause();
+                    CurrentPosition = mediaPlayer.getCurrentPosition();
+                }
+                break;
+            case R.id.stopButton:
+                if(mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer = null;
+                }
+                break;
+
+        }
     }
 }
