274293febe1b3ab3f12ffb982e1980a0847fa3cd
mefahimrahman <mefahimrahman@gmail.com>
2021-01-07 21:24:52 +0600

    SeekBar Added
    1. Seekbar Added by which user can drag the play time.
    2. Play time displayed in two Textview.
    3. One is what time of the song is playing.
    4. Second is the Full song Length.

--- app/src/main/java/com/mefahimrahman/bmplayer/MainActivity.java
+++ app/src/main/java/com/mefahimrahman/bmplayer/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton play, pause, stop;
     MediaPlayer mediaPlayer;
     int CurrentPosition;
+    TextView tvTime, tvDuration;
+    SeekBar seekBarTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         play = (ImageButton) findViewById(R.id.playButton);
         pause = (ImageButton) findViewById(R.id.pauseButton);
         stop = (ImageButton) findViewById(R.id.stopButton);
+        tvTime = (TextView) findViewById(R.id.leftTime);
+        tvDuration = (TextView) findViewById(R.id.rightTime);
+        seekBarTime = (SeekBar) findViewById(R.id.seekBarTime);
 
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
+
+        mediaPlayer  = MediaPlayer.create(getApplicationContext(), R.raw.got);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.seekTo(0);
+
+//      Set Right Side Time
+        String Duration = millisecondsToString(mediaPlayer.getDuration());
+        tvDuration.setText(Duration);
+
+        seekBarTime.setMax(mediaPlayer.getDuration());
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    CurrentPosition = progress;
+                    mediaPlayer.seekTo(progress);
+                    seekBar.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mediaPlayer != null) {
+                    if(mediaPlayer.isPlaying()) {
+                        try {
+                            final double current = mediaPlayer.getCurrentPosition();
+                            String elapsedTime = millisecondsToString((int) current);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTime.setText(elapsedTime);
+                                    seekBarTime.setProgress((int) current);
+                                }
+                            });
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            Log.e("Exception","In Catch Thread");
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -34,6 +97,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.playButton:
                 if(mediaPlayer == null) {
                     mediaPlayer  = MediaPlayer.create(getApplicationContext(), R.raw.got);
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (mediaPlayer != null) {
+                                if(mediaPlayer.isPlaying()) {
+                                    try {
+                                        final double current = mediaPlayer.getCurrentPosition();
+                                        String elapsedTime = millisecondsToString((int) current);
+
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                tvTime.setText(elapsedTime);
+                                                seekBarTime.setProgress((int) current);
+                                            }
+                                        });
+                                        Thread.sleep(1000);
+                                    } catch (InterruptedException e) {
+                                        Log.e("Exception","In Catch Thread");
+                                    }
+                                }
+                            }
+                        }
+                    }).start();
+
                     mediaPlayer.start();
                 }
                 else if(!mediaPlayer.isPlaying()) {
@@ -50,10 +139,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stopButton:
                 if(mediaPlayer != null) {
                     mediaPlayer.stop();
+                    tvTime.setText("0:00");
+                    seekBarTime.setProgress(0);
                     mediaPlayer = null;
                 }
                 break;
 
         }
+    }
+
+    public String millisecondsToString(int time) {
+        String elapsedTime = "";
+        int minutes = time / 1000 / 60;
+        int seconds = time / 1000 % 60;
+        elapsedTime = minutes+":";
+        if(seconds < 10) {
+            elapsedTime += "0";
+        }
+        elapsedTime += seconds;
+
+        return  elapsedTime;
     }
 }
